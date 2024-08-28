af9e0d191f55e3ec119d951039f6055c67fb798d
Tiff0321 <2325287709@qq.com>
2024-08-28 21:52:26 +0900

    修复XSS注入漏洞

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -9,8 +9,10 @@ use App\Models\Topic;
 
 class TopicObserver
 {
-    public function saving(Topic $topic)
+    public function saving(Topic $topic): void
     {
+        // 使用 mews/purifier 来过滤内容，防止 XSS 攻击
+        $topic->body = clean($topic->body, 'user_topic_body');
         $topic->excerpt = make_excerpt($topic->body);
     }
 
