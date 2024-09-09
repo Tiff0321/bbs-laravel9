49fb01fd17403834d74fa0db0ca5927918270dee
Tiff0321 <2325287709@qq.com>
2024-09-10 00:08:46 +0900

    分类话题列表侧边栏资源链接显示

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Link;
 use App\Models\Topic;
 use App\Models\User;
 use Illuminate\Contracts\Foundation\Application;
@@ -19,9 +20,10 @@ class CategoriesController extends Controller
      * @param Request $request
      * @param Topic $topic
      * @param User $user
+     * @param Link $link
      * @return Application|Factory|View
      */
-    public function show(Category $category,Request $request, Topic $topic,User $user): View|Factory|Application
+    public function show(Category $category,Request $request, Topic $topic,User $user, Link $link): View|Factory|Application
     {
         // 读取分类 ID 关联的话题，并按每 20 条分页
         $topics = $topic->withOrder($request->order)
@@ -31,9 +33,11 @@ class CategoriesController extends Controller
 
         // 活跃用户列表
         $active_users = $user->getActiveUsers();
+        // 资源链接
+        $links = $link->getAllCached();
 
         // 传参变量话题和分类到模板中
-        return view('topics.index', compact('topics', 'category','active_users'));
+        return view('topics.index', compact('topics', 'category','active_users', 'links'));
 
     }
 }
