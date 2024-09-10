5f8417a660b7bb2a28120bfc0d05d5ab2d4715c5
Tiff0321 <2325287709@qq.com>
2024-09-10 09:06:28 +0900

    修改

--- app/Http/Middleware/RecordLastActivatedTime.php
+++ app/Http/Middleware/RecordLastActivatedTime.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 
-class RecordLastActivedTime
+class RecordLastActivatedTime
 {
     /**
      * Handle an incoming request.
